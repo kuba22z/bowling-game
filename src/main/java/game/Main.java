234d8d1ed21b16b9c92d647207b8463dfa9c63d2
Jakub Naumowicz <jakub.naumowicz@gmx.de>
2025-03-30 13:26:05 +0200

    prompt score and validate

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,7 +1,33 @@
 package game;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final var game = new BowlingGameState();
 
+        final var score = promptValidScore(scanner, "Enter 1.roll: ", 0, 10);
+
+        scanner.close();
+    }
+
+    private static int promptValidScore(Scanner scanner, String message, int min, int max) {
+        while (true) {
+            System.out.print(message);
+            if (scanner.hasNextInt()) {
+                final var score = scanner.nextInt();
+                if (isInputValid(score, min, max)) {
+                    return score;
+                }
+            } else {
+                scanner.next();
+            }
+            System.out.printf("Please enter a number between %s and %s%n", min, max);
+        }
+    }
+
+    private static boolean isInputValid(int score, int min, int max) {
+        return score >= min && score <= max;
     }
 }
