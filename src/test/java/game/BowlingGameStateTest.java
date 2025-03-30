638d56db04844282fd3c392536908a11043c0a77
Jakub Naumowicz <jakub.naumowicz@gmx.de>
2025-03-30 12:33:24 +0200

    add more test cases

--- src/test/java/game/BowlingGameStateTest.java
+++ src/test/java/game/BowlingGameStateTest.java
@@ -12,7 +12,10 @@ class BowlingGameStateTest {
     static Stream<Arguments> calculateScoreTestArguments()
     {
         return Stream.of(
-                Arguments.arguments(List.of(1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6), 133)
+                Arguments.arguments(List.of(1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6), 133),
+                Arguments.arguments(List.of(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10), 300),
+                Arguments.arguments(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 0),
+                Arguments.arguments(List.of(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), 20)
         );
     }
 
